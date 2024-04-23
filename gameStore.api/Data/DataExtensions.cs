a14e214d32040e23dd4ac068c8bfceb1057be089
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2024-04-23 15:37:43 +0200

    first commit

---
+++ gameStore.api/Data/DataExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace gameStore.api.Data;
+
+public static class DataExtensions
+{
+    public static async Task MigrateDbAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+
+}
---
+++ gameStore.api/Entities/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace gameStore.api.Entities;
+
+public class Genre
+{
+    public int Id { get; set; } 
+
+    public required string Name { get; set; }
+
+}
